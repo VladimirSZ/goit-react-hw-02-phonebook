a525729fb677a98e3a148784c03219c0cd84bae4
VladimirSZ <zyryanov.vladimir.s@gmail.com>
2020-05-18 00:51:00 +0300

    Updates

---
+++ precache-manifest.963d6faf0a048478a424403746b93e0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5e3163acbf91abb8a7691ba45359df",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "62e6800b0156bbc79e23",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e67f2da9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e67f2da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695564a2255b42501d7b",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.a34a3884.chunk.js"
+  },
+  {
+    "revision": "c73043600cf8c264972f",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.2d9dc3c9.js"
+  }
+]);
